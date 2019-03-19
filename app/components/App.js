88abb6444d58198bac9c7ace247b3fd92fce2f41
Umpoy <iumpoy@yahoo.com>
2019-03-19 13:55:21 -0700

    imported stopwatch component

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Stopwatch from './stopwatch';
+
 export default class App extends Component {
   render() {
     return (
       <View className="container" style={styles.container}>
         <Text className="title" style={styles.title}>Stopwatch</Text>
+        <Stopwatch />
       </View>
     )
   }
