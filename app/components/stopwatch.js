2bb910cdd769309b395631ecd96e111ae63042a3
Umpoy <iumpoy@yahoo.com>
2019-03-19 13:52:36 -0700

    created stopwatch component

---
+++ app/components/stopwatch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Stopwatch extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This will be the stopwatch</Text>
+            </View>
+        )
+    }
+}
+
+export default Stopwatch;
