af5b07433242dc84c10717cf67faa2ca54c5f214
Umpoy <iumpoy@yahoo.com>
2019-03-19 13:58:47 -0700

    created constructor and rendered state to DOM

--- app/components/stopwatch.js
+++ app/components/stopwatch.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 class Stopwatch extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: 'stopped',
+            start: null,
+            elapsed: 0
+        }
+    }
+
     render() {
+        const { elapsed, status } = this.state;
         return (
             <View>
-                <Text>This will be the stopwatch</Text>
+                <Text className="elapsed">{elapsed}</Text>
+                <Text className="status">{status}</Text>
             </View>
         )
     }
