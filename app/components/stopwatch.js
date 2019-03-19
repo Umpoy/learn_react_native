f3f10f9fe63bc7244b88835f2417c265c12d89f5
Umpoy <iumpoy@yahoo.com>
2019-03-19 14:20:14 -0700

    created start and stop methods

--- app/components/stopwatch.js
+++ app/components/stopwatch.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class Stopwatch extends Component {
     constructor(props) {
@@ -12,12 +12,35 @@ class Stopwatch extends Component {
         }
     }
 
+    start = () => {
+        this.setState({
+            status: 'running',
+            start: new Date().getTime()
+        });
+    }
+
+    stop = () => {
+        this.setState({ status: 'stopped' });
+    }
+    
     render() {
         const { elapsed, status } = this.state;
         return (
             <View>
                 <Text className="elapsed">{elapsed}</Text>
                 <Text className="status">{status}</Text>
+                <Button
+                    onPress={this.start}
+                    title="Start"
+                    color="#841584"
+                    accessibilityLabel="Start Timer"
+                />
+                <Button
+                    onPress={this.stop}
+                    title="Stop"
+                    color="#841584"
+                    accessibilityLabel="Stop Timer"
+                />
             </View>
         )
     }
