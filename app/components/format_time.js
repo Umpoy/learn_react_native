76a914d2805d2d7ffa073c51cdacf94e3f92e62e
Umpoy <iumpoy@yahoo.com>
2019-03-19 19:44:24 -0700

    created method to make time readable

--- app/components/format_time.js
+++ app/components/format_time.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class FormatTime extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    millisecondToTime = () => {
+        const { elapsed } = this.props;
+        const sec = elapsed / 1000;
+        const min = sec / 60;
+        return {
+            hour: Math.floor(min / 60),
+            min: Math.floor(min % 60),
+            sec: Math.floor(sec % 60),
+            ms: Math.floor(elapsed % 100)
+        }
+    }
+
     render() {
         return (
             <Text>{this.props.elapsed}</Text>
